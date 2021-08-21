d67e23984187cafb46b0bdfa98efab9e0915ac9e
praveenkumar-4E2 <pthanneru@gmail.com>
2021-08-21 22:07:53 +0530

    kchage

--- fooling/settings.py
+++ fooling/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-$7dd6p5_8*@vq)5gpwh#=8ri&qs9n^u=6!9%mq6yc%5894m2tt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
